9d0e47a6690c7c9fa16a4b0970ef07907f769ca8
Najam Ul Hassan <124649431+Nuhk2@users.noreply.github.com>
2025-11-12 00:00:29 +0500

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Portfolio: React.FC = () => (
   <section id="projects" className="">
-    <h2  className="text-5xl font-silk md:text-7xl text-gold text-center ">Portfolio</h2>
+    <h2  className="text-5xl font-silk md:text-7xl text-gold text-center pt-20">Portfolio</h2>
 <p className="font-silk text-gold text-center pl-32 pr-32 pt-5 pb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni est, mollitia temporibus repudiandae aliquid molestias totam deserunt, vel praesentium omnis provident consectetur! Voluptatem vitae minus nemo sint. Tempore aspernatur impedit nisi a ducimus beatae quis, repudiandae cumque dolorem dolorum dolores vitae amet, error culpa fuga. Tempora iusto accusamus ipsa corrupti magni voluptatem delectus asperiores debitis a culpa laboriosam, aperiam et in necessitatibus magnam tenetur quia quod doloribus! In, rem optio aspernatur distinctio suscipit quaerat maiores autem repudiandae! Pariatur totam fugit iure sapiente nulla obcaecati vitae natus quas sequi inventore ea officiis est error adipisci molestias facilis porro cupiditate quis quisquam ipsa, officia, ullam possimus, exercitationem labore! Velit ullam labore iste, maiores vel ex asperiores neque sapiente. Fuga praesentium officiis hic!</p>
   </section>
   
