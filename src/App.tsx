e022e2b38d6f50ca401b18cff29419d21fd5a6fe
Najam Ul Hassan <124649431+Nuhk2@users.noreply.github.com>
2025-11-13 19:46:59 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 // import Hero from "./components/Hero";
 import Portfolio from "./components/Portfolio";
